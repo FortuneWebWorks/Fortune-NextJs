2f754969582ffbc479f9258c8ccda040ab6f309e
lifeloop2180 <lifeloop2180@gmail.com>
2022-03-26 07:23:39 +0430

    :zap: sass added to project now we are able to right sass just linke css module and import them in pages, there was a missing / in jsconfig for importing with @ and that fixed

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import Layout from '@components/Layout';
+import Layout from '@/components/Layout';
+import styles from '@/styles/Home.module.scss';
 
 export default function Home() {
   return (
     <Layout>
-      <div>
-        <h1>Hello</h1>
+      <div className={styles.container}>
+        <div className={styles.main}>
+          <h1>Hello</h1>
+        </div>
       </div>
     </Layout>
   );

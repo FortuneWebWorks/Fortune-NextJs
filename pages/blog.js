a9b7260851a0c579c940c2382ccfb8413878c45a
lifeloop2180 <lifeloop2180@gmail.com>
2022-03-29 11:53:25 +0430

    :zap: blog  and form page added with styles

---
+++ pages/blog.js
@@ -0,0 +1,85 @@
+import Layout from '@/components/Layout';
+import styles from '@/styles/Blog.module.scss';
+
+function blog() {
+  return (
+    <Layout>
+      <section className={styles.blog_body}>
+        <div className={styles.blog_post}>
+          <h2>
+            THE COMPLETE TUTORIAL OF HOW TO BE FUCKING AMAZING IN ALL OF THE
+            UNIVERSE
+          </h2>
+          <div className={styles.blog_post_info}>
+            <span className={styles.dude}>
+              <span className={styles.point_circle}>🟠</span> one of us
+            </span>{' '}
+            <span className={styles.date}>2022 28 2</span>
+          </div>
+          <p className={styles.blog_post_comment}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+            harum repellendus culpa autem alias similique ipsam cum modi
+            voluptates, molestiae, qui omnis, fuga sequi assumenda consectetur
+            facilis. Excepturi, cumque dolorum.
+          </p>
+        </div>
+        <div className={styles.blog_post}>
+          <h2>
+            THE COMPLETE TUTORIAL OF HOW TO BE FUCKING AMAZING IN ALL OF THE
+            UNIVERSE
+          </h2>
+          <div className={styles.blog_post_info}>
+            <span className={styles.dude}>
+              <span className={styles.point_circle}>🟠</span> one of us
+            </span>{' '}
+            <span className={styles.date}>2022 28 2</span>
+          </div>
+          <p className={styles.blog_post_comment}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+            harum repellendus culpa autem alias similique ipsam cum modi
+            voluptates, molestiae, qui omnis, fuga sequi assumenda consectetur
+            facilis. Excepturi, cumque dolorum.
+          </p>
+        </div>
+        <div className={styles.blog_post}>
+          <h2>
+            THE COMPLETE TUTORIAL OF HOW TO BE FUCKING AMAZING IN ALL OF THE
+            UNIVERSE
+          </h2>
+          <div className={styles.blog_post_info}>
+            <span className={styles.dude}>
+              <span className={styles.point_circle}>🟠</span> one of us
+            </span>{' '}
+            <span className={styles.date}>2022 28 2</span>
+          </div>
+          <p className={styles.blog_post_comment}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+            harum repellendus culpa autem alias similique ipsam cum modi
+            voluptates, molestiae, qui omnis, fuga sequi assumenda consectetur
+            facilis. Excepturi, cumque dolorum.
+          </p>
+        </div>
+        <div className={styles.blog_post}>
+          <h2>
+            THE COMPLETE TUTORIAL OF HOW TO BE FUCKING AMAZING IN ALL OF THE
+            UNIVERSE
+          </h2>
+          <div className={styles.blog_post_info}>
+            <span className={styles.dude}>
+              <span className={styles.point_circle}>🟠</span> one of us
+            </span>{' '}
+            <span className={styles.date}>2022 28 2</span>
+          </div>
+          <p className={styles.blog_post_comment}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+            harum repellendus culpa autem alias similique ipsam cum modi
+            voluptates, molestiae, qui omnis, fuga sequi assumenda consectetur
+            facilis. Excepturi, cumque dolorum.
+          </p>
+        </div>
+      </section>
+    </Layout>
+  );
+}
+
+export default blog;
---
+++ pages/form.js
@@ -0,0 +1,69 @@
+import Image from 'next/image';
+import styles from '@/styles/Form.module.scss';
+import headStyle from '@/styles/Header.module.scss';
+
+function form() {
+  return (
+    <>
+      <header
+        className={headStyle.header}
+        style={{ justifyContent: 'center', height: '5rem', padding: '16px' }}
+      >
+        <div className={headStyle.head_content_1}>
+          <div className={headStyle.logo}>
+            <Image
+              src="http://localhost:3000/favicon.jpg"
+              alt="logo"
+              objectFit="cover"
+              width="100"
+              height="100"
+            />
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className={styles.form}>
+          <form id="form">
+            <h2>
+              PLEASE INFORM US OF YOUR WEBSITE AND WE WILL SEND YOU THE
+              ANALYSIS:
+            </h2>
+
+            <div className={styles.info}>
+              <div>
+                <label htmlFor="name">your name:</label>
+                <input type="text" name="name" id="name" required />
+              </div>
+
+              <div>
+                <label htmlFor="email">your email:</label>
+                <input type="email" name="email" id="email" required />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="website">your website address:</label>
+              <input type="url" name="website" id="website" required />
+            </div>
+
+            <div>
+              <label htmlFor="discuss">
+                anything that you want to discuss:
+              </label>
+              <textarea
+                id="discuss"
+                className={styles.discuss}
+                name="discuss"
+              ></textarea>
+            </div>
+
+            <button className={styles.submit}>submit ✉️</button>
+          </form>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default form;

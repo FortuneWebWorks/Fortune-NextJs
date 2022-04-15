09a996ea4380ed1af11fa9537e2046c7cb44627e
lifeloop2180 <lifeloop2180@gmail.com>
2022-04-15 12:52:07 +0430

    :zap: workinng on vercel problem

--- components/HatAnimation.js
+++ components/HatAnimation.js
@@ -1,8 +1,9 @@
 /** @type {HTMLCanvasElement} */
 import { useRef, useEffect } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
 import style from '@/styles/Hat.module.scss';
-import { useRouter } from 'next/router';
+import Router, { useRouter } from 'next/router';
 
 function HatAnimation() {
   const router = useRouter();
@@ -15,6 +16,13 @@ function HatAnimation() {
   let stars = [];
   const limit = 70;
 
+  // Router.events.on('routeChangeStart', () => {
+  //   stars = [];
+  //   cancelAnimationFrame(animation);
+  //   clearInterval(interval);
+  //   return;
+  // });
+
   useEffect(() => {
     clearInterval(interval);
     cancelAnimationFrame(animation);
@@ -22,13 +30,6 @@ function HatAnimation() {
     canvas.current.width = 200;
     canvas.current.height = 500;
     c.current = canvas.current.getContext('2d');
-
-    if (stars.length < limit) {
-      c.current.clearRect(0, 0, canvas.current.width, canvas.current.height);
-      cancelAnimationFrame(animation);
-
-      startBuilding();
-    }
 
     let interval = setInterval(() => {
       cancelAnimationFrame(animation);
@@ -49,7 +50,6 @@ function HatAnimation() {
 
       clearInterval(interval);
       cancelAnimationFrame(animation);
-      c.current.clearRect(0, 0, canvas.current.width, canvas.current.height);
     };
     window.addEventListener('blur', onBlur);
 
@@ -57,12 +57,12 @@ function HatAnimation() {
       // eslint-disable-next-line react-hooks/exhaustive-deps
       window.removeEventListener('focus', onFocus);
       window.removeEventListener('blur', onBlur);
-      // stars = [];
+      stars = [];
 
       clearInterval(interval);
       cancelAnimationFrame(animation);
     };
-  });
+  }, [stars]);
 
   class Star {
     constructor(x, y, dx, dy, size, fadeTime, id) {
@@ -160,11 +160,11 @@ function HatAnimation() {
         c.current.clearRect(0, 0, canvas.current.width, canvas.current.height);
         cancelAnimationFrame(animation);
 
+        startBuilding();
+
         if (router.pathname !== '' && router.pathname !== '/') {
           router.push('/');
         }
-
-        startBuilding();
       }
     }
   };

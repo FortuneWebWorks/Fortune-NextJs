3a888450b6c6dd41401d673e862e81e49661cfad
lifeloop2180 <lifeloop2180@gmail.com>
2022-04-16 10:16:51 +0430

    :zap: Notion connected to blog

--- next.config.js
+++ next.config.js
@@ -18,6 +18,8 @@ const nextConfig = {
   env: {
     TELEGRAM_BOT_TOKEN: '5186582196:AAF6dIzidTOtJRfwhaAu-hqwIkgdTsbA1zY',
     TELEGRAM_CHAT_ID: '761026981',
+    NOTION_SECRESTS: 'secret_80UTjG2bD30G7BbyMrlJU90trYeGsmpVxCPayst92Jw',
+    NOTION_DATABASE_ID: '465c13b7bbe740e9895a31c0905cea89',
   },
 };
 
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,144 +1,212 @@
-import Image from 'next/image';
-import Router from 'next/router';
+import { Fragment } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
 import styles from '@/styles/Blog.module.scss';
-import headStyle from '@/styles/Header.module.scss';
-import footerStyle from '@/styles/Footer.module.scss';
+import { getDatabase, getBlocks } from '@/lib/notion';
 import Layout from '@/components/Layout';
 
-function blog() {
-  const imageClick = () => {
-    Router.push('/');
-  };
+export const Text = ({ text }) => {
+  if (!text) {
+    return null;
+  }
+
+  return text.map((value, index) => {
+    const {
+      annotations: { bold, code, color, italic, strikethrough, underline },
+      text,
+    } = value;
+    return (
+      <span
+        key={index}
+        className={[
+          bold ? styles.bold : '',
+          code ? styles.code : '',
+          italic ? styles.italic : '',
+          strikethrough ? styles.strikethrough : '',
+          underline ? styles.underline : '',
+        ].join(' ')}
+        style={color !== 'default' ? { color } : {}}
+      >
+        {text.link ? <a href={text.link.url}>{text.content}</a> : text.content}
+      </span>
+    );
+  });
+};
+
+const renderNestedList = (block) => {
+  const { type } = block;
+  const value = block[type];
+  if (!value) return null;
+
+  const isNumberedList = value.children[0].type === 'numbered_list_item';
+
+  if (isNumberedList) {
+    return <ol>{value.children.map((block) => renderBlock(block))}</ol>;
+  }
+  return <ul>{value.children.map((block) => renderBlock(block))}</ul>;
+};
+
+const renderBlock = (block) => {
+  console.log(block);
+  const { type, id } = block;
+  const value = block[type];
+
+  switch (type) {
+    case 'paragraph':
+      return (
+        <p>
+          <Text text={value.rich_text} />
+        </p>
+      );
+    case 'heading_1':
+      return (
+        <h1>
+          <Text text={value.rich_text} />
+        </h1>
+      );
+    case 'heading_2':
+      return (
+        <h2>
+          <Text text={value.rich_text} />
+        </h2>
+      );
+    case 'heading_3':
+      return (
+        <h3>
+          <Text text={value.rich_text} />
+        </h3>
+      );
+    case 'bulleted_list_item':
+    case 'numbered_list_item':
+      return (
+        <li>
+          <Text text={value.rich_text} />
+          {!!value.children && renderNestedList(block)}
+        </li>
+      );
+    case 'to_do':
+      return (
+        <div>
+          <label htmlFor={id}>
+            <input type="checkbox" id={id} defaultChecked={value.checked} />{' '}
+            <Text text={value.rich_text} />
+          </label>
+        </div>
+      );
+    case 'toggle':
+      return (
+        <details>
+          <summary>
+            <Text text={value.rich_text} />
+          </summary>
+          {value.children?.map((block) => (
+            <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+          ))}
+        </details>
+      );
+    case 'child_page':
+      return <p>{value.title}</p>;
+    case 'image':
+      const src =
+        value.type === 'external' ? value.external.url : value.file.url;
+      console.log(src);
+      const caption = value.caption ? value.caption[0]?.plain_text : '';
+      return (
+        <figure>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={src} alt={caption} />
+          {caption && <figcaption>{caption}</figcaption>}
+        </figure>
+      );
+    case 'divider':
+      return <hr key={id} />;
+    case 'quote':
+      return <blockquote key={id}>{value.rich_text[0].plain_text}</blockquote>;
+    case 'code':
+      return (
+        <pre className={styles.pre}>
+          <code className={styles.code_block} key={id}>
+            {value.rich_text[0].plain_text}
+          </code>
+        </pre>
+      );
+    case 'file':
+      const src_file =
+        value.type === 'external' ? value.external.url : value.file.url;
+      const splitSourceArray = src_file.split('/');
+      const lastElementInArray = splitSourceArray[splitSourceArray.length - 1];
+      const caption_file = value.caption ? value.caption[0]?.plain_text : '';
+      return (
+        <figure>
+          <div className={styles.file}>
+            📎{' '}
+            <Link href={src_file} passHref>
+              {lastElementInArray.split('?')[0]}
+            </Link>
+          </div>
+          {caption_file && <figcaption>{caption_file}</figcaption>}
+        </figure>
+      );
+    case 'bookmark':
+      const href = value.url;
+      return (
+        <a href={href} target="_brank" className={styles.bookmark}>
+          {href}
+        </a>
+      );
+    default:
+      return `❌ Unsupported block (${
+        type === 'unsupported' ? 'unsupported by Notion API' : type
+      })`;
+  }
+};
+
+export default function Post({ blocks, database }) {
+  if (!blocks) {
+    return <div />;
+  }
 
   return (
     <Layout>
-      <section className={styles.blog_body}>
-        {/* <header className={headStyle.header}>
-          <div className={headStyle.head_content_1} style={{ height: '3rem' }}>
-            <div className={headStyle.logo}>
-              <Image
-                src="http://localhost:3000/favicon.jpg"
-                alt="logo"
-                objectFit="cover"
-                width="100"
-                height="100"
-                onClick={imageClick}
-              />
+      <div className={styles.blog_body}>
+        {blocks.map((block, index) => (
+          <Fragment key={block[0].id}>
+            <div className={styles.blog_post}>
+              <h2 className={styles.blog_post_title}>
+                {database[index].properties.title.rich_text[0].text.content}
+              </h2>
+              <div className={styles.blog_post_info}>
+                <span className={styles.dude}>
+                  <span className={styles.point_circle}>🟠</span>
+                  {database[index].properties.Auther.rich_text[0].text.content}
+                </span>
+                <span className={styles.date}>
+                  {database[index].properties.Date.date.start}
+                </span>
+              </div>
+              {renderBlock(block[0])}
             </div>
-          </div>
-          <div
-            className="head-content-2"
-            style={{ margin: 'auto', alignItems: 'center' }}
-          >
-            <span
-              style={{
-                color: '#ff8d4f',
-                fontWeight: 'bold',
-                fontSize: '1.4rem',
-                fontFamily: 'Cubano',
-              }}
-            >
-              THE BLOG
-            </span>
-          </div>
-        </header> */}
-
-        <div className={styles.blog_post}>
-          <h2 className={styles.blog_post_title}>
-            THE COMPLETE TUTORIAL OF HOW TO BE FUCKING AMAZING IN ALL OF THE
-            UNIVERSE
-          </h2>
-          <div className={styles.blog_post_info}>
-            <span className={styles.dude}>
-              <span className={styles.point_circle}>🟠</span> one of us
-            </span>
-            <span className={styles.date}>2022 28 2</span>
-          </div>
-          <p className={styles.blog_post_comment}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-            harum repellendus culpa autem alias similique ipsam cum modi
-            voluptates, molestiae, qui omnis, fuga sequi assumenda consectetur
-            facilis. Excepturi, cumque dolorum.
-          </p>
-        </div>
-        <div className={styles.blog_post}>
-          <h2 className={styles.blog_post_title}>
-            THE COMPLETE TUTORIAL OF HOW TO BE FUCKING AMAZING IN ALL OF THE
-            UNIVERSE
-          </h2>
-          <div className={styles.blog_post_info}>
-            <span className={styles.dude}>
-              <span className={styles.point_circle}>🟠</span> one of us
-            </span>{' '}
-            <span className={styles.date}>2022 28 2</span>
-          </div>
-          <p className={styles.blog_post_comment}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-            harum repellendus culpa autem alias similique ipsam cum modi
-            voluptates, molestiae, qui omnis, fuga sequi assumenda consectetur
-            facilis. Excepturi, cumque dolorum.
-          </p>
-        </div>
-        <div className={styles.blog_post}>
-          <h2 className={styles.blog_post_title}>
-            THE COMPLETE TUTORIAL OF HOW TO BE FUCKING AMAZING IN ALL OF THE
-            UNIVERSE
-          </h2>
-          <div className={styles.blog_post_info}>
-            <span className={styles.dude}>
-              <span className={styles.point_circle}>🟠</span> one of us
-            </span>{' '}
-            <span className={styles.date}>2022 28 2</span>
-          </div>
-          <p className={styles.blog_post_comment}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-            harum repellendus culpa autem alias similique ipsam cum modi
-            voluptates, molestiae, qui omnis, fuga sequi assumenda consectetur
-            facilis. Excepturi, cumque dolorum.
-          </p>
-        </div>
-        <div className={styles.blog_post}>
-          <h2 className={styles.blog_post_title}>
-            THE COMPLETE TUTORIAL OF HOW TO BE FUCKING AMAZING IN ALL OF THE
-            UNIVERSE
-          </h2>
-          <div className={styles.blog_post_info}>
-            <span className={styles.dude}>
-              <span className={styles.point_circle}>🟠</span> one of us
-            </span>{' '}
-            <span className={styles.date}>2022 28 2</span>
-          </div>
-          <p className={styles.blog_post_comment}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-            harum repellendus culpa autem alias similique ipsam cum modi
-            voluptates, molestiae, qui omnis, fuga sequi assumenda consectetur
-            facilis. Excepturi, cumque dolorum.
-          </p>
-        </div>
-
-        {/* <footer className={footerStyle.footer} style={{ height: '4rem' }}>
-          <div
-            className={footerStyle.footer_content_1}
-            style={{ height: '3rem' }}
-          >
-            <div className={footerStyle.logo} style={{ position: 'relative' }}>
-              <Image
-                src="http://localhost:3000/favicon.jpg"
-                alt="logo"
-                objectFit="cover"
-                layout="fill"
-              />
-            </div>
-          </div>
-          <div className={footerStyle.footer_content_2}>
-            <span>blog</span>
-            <span>twitter</span>
-            <span>upwork</span>
-          </div>
-        </footer> */}
-      </section>
+          </Fragment>
+        ))}
+      </div>
     </Layout>
   );
 }
 
-export default blog;
+export const getStaticProps = async () => {
+  const database = await getDatabase(process.env.NOTION_DATABASE_ID);
+
+  const blocks = await Promise.all(
+    database.map(async (item) => {
+      return await getBlocks(item.id);
+    })
+  );
+
+  return {
+    props: {
+      database,
+      blocks,
+    },
+    revalidate: 1,
+  };
+};

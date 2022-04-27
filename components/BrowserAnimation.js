79e9296cca4fcf1dc7a52a9b98b89dab2839107f
lifeloop2180 <lifeloop2180@gmail.com>
2022-04-27 14:47:24 +0430

    :zap: Section2 animation added and load error added to iframe

--- components/BrowserAnimation.js
+++ components/BrowserAnimation.js
@@ -1,40 +1,173 @@
-import LfetExplodingBrowser from './LfetExplodingBrowser';
+import { useRef, useEffect } from 'react';
 import styles from '@/styles/Browsers.module.scss';
-import CodeEditors from './CodeEditors';
+import editorsStyle from '@/styles/CodeEditors.module.scss';
+import Exploder from './LfetExplodingBrowser';
+import { gsap, SteppedEase } from 'gsap/dist/gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 function BrowserAnimation() {
+  let leftBrowser = useRef(null);
+  let rightBrowser = useRef(null);
+  let codeEditors = useRef(null);
+  let firePlace = useRef(null);
+  let behindText = useRef(null);
+  let typer1 = useRef(null);
+  let typer2 = useRef(null);
+  let canvas = useRef(null);
+  let animation = useRef(null);
+  let exploders = useRef([]);
+  let c = useRef();
+
+  useEffect(() => {
+    let counter = 0;
+    const tl = gsap.timeline();
+    // Canvas Things
+    c.current = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 800;
+
+    const fire = () => {
+      exploders.current.forEach((item) => {
+        item.setFire = true;
+      });
+      animate();
+
+      gsap.to(behindText, {
+        y: '-50%',
+        opacity: 1,
+      });
+
+      setTimeout(() => {
+        gsap.to(codeEditors, {
+          position: 'static',
+          opacity: 1,
+        });
+
+        setTimeout(() => {
+          if (counter <= 0) {
+            typing(typer1);
+            chartsFade();
+            counter++;
+          }
+        }, 2500);
+      }, 2000);
+
+      tl.to(rightBrowser, {
+        y: '30%',
+      });
+
+      if (counter <= 0) {
+        gsap.to('.fadein', { opacity: 0 });
+      }
+    };
+
+    const leave = () => {
+      gsap.to(behindText, {
+        y: '100%',
+        duration: 0.5,
+      });
+      gsap.to(behindText, {
+        opacity: 0,
+        duration: 0,
+      });
+    };
+
+    const animate = () => {
+      if (!canvas) return;
+      animation.current = requestAnimationFrame(animate);
+
+      c.current.clearRect(0, 0, canvas.width, canvas.height);
+
+      exploders.current.forEach((item) => {
+        item.update();
+      });
+    };
+
+    const typing = (target) => {
+      let text = `
+      <div>Test</div>
+      <p>Hello world</p>
+      <span>Opps</span>
+    `;
+
+      let curr = 0;
+
+      let Write = function write() {
+        let elem = target;
+
+        elem.textContent = elem.textContent + text.charAt(curr);
+
+        curr++;
+        if (typer1.textContent.length - 1 < text.length && curr < text.length) {
+          window.setTimeout(write, 10);
+        } else if (typer2.textContent === '') {
+          typing(typer2);
+        }
+      };
+
+      Write();
+    };
+
+    const chartsFade = () => {
+      gsap.to('.fadein', { opacity: 1, stagger: 2 });
+    };
+
+    for (let i = 0; i < 200; i++) {
+      const dx = parseFloat((Math.random() * 10 - 5).toFixed(7));
+      const dy = parseFloat((Math.random() * 10 - 5).toFixed(7));
+
+      exploders.current.push(
+        new Exploder(
+          canvas,
+          exploders.current,
+          canvas.width / 2,
+          canvas.height / 2,
+          dx,
+          dy,
+          1
+        )
+      );
+    }
+
+    // Animation
+    tl.to(leftBrowser, {
+      scrollTrigger: {
+        trigger: firePlace,
+        start: 'top top',
+        onEnter: fire,
+        onLeaveBack: leave,
+      },
+      opacity: 0,
+      onComplete() {
+        gsap.to(leftBrowser, {
+          display: 'none',
+        });
+      },
+    });
+  }, []);
+
   return (
     <section className={`section2 ${styles.section2}`} id="section-2">
       <h2
         className={styles.text}
+        ref={(el) => (firePlace = el)}
         style={{ transition: 'opacity 0.5s ease-out' }}
       >
         Say goodbye to bloated and overly complicated solutions— web design
         shouldn’t be a riddle.
-      </h2>
-      <h2
-        className={styles.text}
-        id="fire-place"
-        style={{ transition: 'opacity 0.5s ease-out' }}
-      >
-        Delivery of fast and reliable services, like pulling a rabbit out of a
-        hat.
       </h2>
       <div
         className={styles.section2_browsers}
         id="section2-scroll"
         style={{ transition: 'opacity 0.5s ease-out', overflow: 'visible' }}
       >
-        <div
-          className={`section2-icnon ${styles.first_browser}`}
-          id="code-editors-container"
-          style={{
-            position: 'relative',
-            overflow: 'visible',
-          }}
-        >
+        <div className={`section2-icnon ${styles.first_browser}`}>
           <div
             id="explod_target"
+            ref={(el) => {
+              leftBrowser = el;
+            }}
             style={{
               position: 'relative',
               transition: 'all 500ms ease-out',
@@ -60,7 +193,7 @@ function BrowserAnimation() {
               src="https://drive.google.com/uc?export=view&id=1JZnd3ri1q5A02BfC1cIAqeDqaY3gXTLl"
               alt="Loading..."
               style={{
-                width: '30%',
+                width: '20%',
                 position: 'absolute',
                 top: '50%',
                 left: '50%',
@@ -68,11 +201,55 @@ function BrowserAnimation() {
               }}
             />
           </div>
-          <LfetExplodingBrowser />
-          <CodeEditors />
+          <canvas
+            className={styles.exploder}
+            ref={(el) => (canvas = el)}
+          ></canvas>
+          {/* Code editors */}
+          <div
+            ref={(el) => (codeEditors = el)}
+            id="code-editors"
+            style={{
+              opacity: '0',
+              position: 'absolute',
+              zIndex: '-100',
+              transition: 'opacity 1s 1s ease-in',
+            }}
+            className={editorsStyle.container}
+          >
+            <div className={editorsStyle.editors_container}>
+              <div>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+              <div>
+                <span>index.html</span>
+              </div>
+
+              <div
+                id="html"
+                className={styles.html}
+                ref={(el) => (typer1 = el)}
+              ></div>
+            </div>
+            <div className={editorsStyle.editors_container}>
+              <div>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+              <div>main.css</div>
+              <div id="css" ref={(el) => (typer2 = el)}></div>
+            </div>
+          </div>
         </div>
 
-        <div className={styles.second_browser} id="second-browser">
+        <div
+          className={styles.second_browser}
+          id="second-browser"
+          ref={(el) => (rightBrowser = el)}
+        >
           <div className="section2-searchbar" style={{ height: '100%' }}>
             <div className="buttons">
               <div className="btn"></div>
@@ -85,7 +262,7 @@ function BrowserAnimation() {
               <div></div>
             </div>
             <div className={styles.content} id="animate-target">
-              <div className={styles.dashboard}>
+              <div className={`${styles.dashboard} fadein`}>
                 <h5>
                   <span>🔥</span>DASHBOARD
                 </h5>
@@ -98,7 +275,7 @@ function BrowserAnimation() {
                   <li>blog posts</li>
                 </ul>
               </div>
-              <div className={styles.charts_container}>
+              <div className={`${styles.charts_container} fadein`}>
                 <div>
                   {/* eslint-disable-next-line @next/next/no-img-element */}
                   <img
@@ -123,7 +300,14 @@ function BrowserAnimation() {
           </div>
         </div>
       </div>
-      {/* <HatRabit /> */}
+      <h2
+        className={`${styles.text} ${styles.fireText}`}
+        ref={(el) => (behindText = el)}
+        style={{ transition: 'opacity 0.5s ease-out' }}
+      >
+        Delivery of fast and reliable services, like pulling a rabbit out of a
+        hat.
+      </h2>
     </section>
   );
 }
--- components/Youtube.js
+++ components/Youtube.js
@@ -1,8 +1,38 @@
 import styles from '@/styles/Youtube.module.scss';
+import { useEffect, useRef } from 'react';
 
 const Youtube = () => {
+  const iframe = useRef(null);
+
+  useEffect(() => {
+    function setIframeSrc() {
+      setTimeout(function () {
+        if (window.stop) {
+          window.stop();
+          iframe.innerHTML = `<h2>Faild to load content! please check your connection....</h2>`;
+          iframe.style.border = '1px solid coral';
+        } else {
+          document.execCommand('Stop');
+          iframe.innerHTML = `<h2>Faild to load content! please check your connection....</h2>`;
+          iframe.style.border = '1px solid coral';
+        }
+      }, 5000);
+    }
+    setTimeout(setIframeSrc, 5000);
+  }, []);
+
   return (
-    <div className={styles.youtube}>
+    <div
+      className={styles.youtube}
+      ref={(el) => (iframe = el)}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        borderRadius: '7px',
+      }}
+    >
       <iframe
         src="https://youtube.com/embed/HF1SgsoHE-g"
         width="300"
